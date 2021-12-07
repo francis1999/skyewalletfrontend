06c178dce43ddc235c8bd6c8ed58cf8bbb0f33f6
francis1999 <gbade.francis@gmail.com>
2021-12-07 22:01:28 +0000

    first commit on skye wallet App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="registration">
+        <form>
+            <label></label>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
